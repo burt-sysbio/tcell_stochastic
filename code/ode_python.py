b3d92cdcb3febe33f45f908181cbcdc467a509e3
burt-sysbio <philippburt@gmail.com>
2020-07-03 12:09:52 +0200

    added timer to stoc model and ODE model in python

---
+++ code/ode_python.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 09:40:30 2020
+
+@author: Philipp
+"""
+
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from scipy.integrate import odeint
+
+sns.set(style = "ticks", context = "poster")
+
+d = {
+    "alpha" : 2,
+    "beta" : 2,
+    "deg_myc" : 0.5,
+    "r_div" : 0,
+    "r_div_th1" : 1,
+    "EC50_myc" : 0.5,
+    "r_death" : 1,
+    "prob_th1" : 0.5,
+    "prob_tfh" : 0.2,
+    "prob_tr1" : 0.3,
+    }
+
+def pos_fb(c, EC50):
+    return (c/(c+EC50))
+
+
+def ode_model(state, time, d):
+    naive = state[0]
+    naive_1 = state[1]
+    th1 = state[2]
+    tfh = state[3]
+    tr1 = state[4]
+    
+    myc = np.exp(-d["deg_myc"]*time)
+    r_div_th1 = d["r_div_th1"]*pos_fb(myc, d["EC50_myc"])
+    
+    dt_naive = -d["beta"]*naive
+    dt_naive_1 = d["beta"]*(naive-naive_1)
+    dt_th1 = d["beta"]*naive_1*d["prob_th1"]+(r_div_th1-d["r_death"])*th1
+    dt_tfh = d["beta"]*naive_1*d["prob_tfh"]+(d["r_div"]-d["r_death"])*tfh
+    dt_tr1 = d["beta"]*naive_1*d["prob_tr1"]+(d["r_div"]-d["r_death"])*tr1
+    
+    dt_state = [dt_naive, dt_naive_1, dt_th1, dt_tfh, dt_tr1]
+    return dt_state
+
+
+t = np.arange(0,10,0.01)
+y0 = np.zeros(5)
+y0[0] = 1
+state = odeint(ode_model, y0, t, args = (d,))
+
+df = pd.DataFrame(state, columns = ["naive", "naive_1", "Th1", "Tfh", "Tr1"])
+df = df[["Th1", "Tfh", "Tr1"]]
+df["time"] = t
+
+n_cells = 1000
+df_tidy = df.melt(id_vars = ["time"])
+df_tidy.value = df_tidy.value*n_cells
+df_tidy["model"] = "ODE"
+
+df_stocsim = pd.read_csv("../output/teststoring.csv")
+df_tidy_stoc = df_stocsim.melt(id_vars = ["time"])
+df_tidy_stoc["model"] = "Stoc."
+
+df_all = pd.concat([df_tidy, df_tidy_stoc])
+g = sns.relplot(data = df_all, x = "time", y = "value", hue = "variable", 
+                kind = "line", style = "model")
--- code/stochastic_model.jl
+++ code/stochastic_model.jl
@@ -2,8 +2,9 @@
 
 using Distributions
 using DataFrames
+using CSV
 
-function draw_fate(p1=0.2, p2=0.5, p3=0.3)
+function draw_fate(p1=0.5, p2=0.2, p3=0.3)
     # draw random number
     n = rand(Float64)
     if 0 <= n < p1
@@ -17,8 +18,21 @@ function draw_fate(p1=0.2, p2=0.5, p3=0.3)
 end
 
 
+function pos_fb(c, EC50)
+    return c/(c+EC50)
+end
+
+function get_myc(time, deg_myc)
+    return exp(-deg_myc*time)
+end
 # for each time point loop over each cell
 function stoc_model(n_cells, time_arr)
+
+    alpha = 2
+    beta = 2
+    r_div_base = 1
+    EC50_myc = 0.5
+    deg_myc = 0.5
     # assign indices
     naive_idx = 0
     th1_idx = 1
@@ -31,10 +45,11 @@ function stoc_model(n_cells, time_arr)
     prob_state_change = 3
     prob_death = 4
     prob_div = 5
-    last_death = 6
-    last_div = 7
+    prob_div0 = 6
+    last_death = 7
+    last_div = 8
 
-    n_states = 7
+    n_states = 8
     cell_arr = zeros((n_cells, n_states))
     # assign random numbers for naive prec. transition
     cell_arr[:,prob_state_change] = rand(n_cells)
@@ -42,7 +57,7 @@ function stoc_model(n_cells, time_arr)
     cell_arr[:,prob_death] = rand(n_cells)
     cell_arr[:,prob_div] = rand(n_cells)
 
-    n_dead_cells = 1000
+    n_dead_cells = 2000
     dead_cell_arr = zeros((n_dead_cells, n_states))
     dead_cell_arr[:, cell_state] .= dead_idx
     cell_arr = [cell_arr; dead_cell_arr]
@@ -54,7 +69,9 @@ function stoc_model(n_cells, time_arr)
 
     for t = 1:(length(time_arr)-1)
         time = time_arr[t]
-
+        t_new = time_arr[t+1]
+        myc = get_myc(time, deg_myc)
+        r_div = r_div_base*pos_fb(myc, EC50_myc)
         # get indices of dead cells
         # use 8 as idx for dead cells
         alive_cells = findall(cell_arr[:,cell_state] .!= dead_idx)
@@ -63,7 +80,7 @@ function stoc_model(n_cells, time_arr)
             # not taking jump instance into account right now cuz naive cells
             # use so that I can increase it
             if cell_arr[i,cell_state] == naive_idx
-                if 1-cdf(Gamma(1,1), time) < cell_arr[i,prob_state_change]
+                if 1-cdf(Gamma(alpha,1/beta), time) < cell_arr[i,prob_state_change]
                     # assign new cell state
                     cell_arr[i,cell_state] = draw_fate()
                     # assign jump time
@@ -82,8 +99,9 @@ function stoc_model(n_cells, time_arr)
             # still not sure if I should use update state at t+1 so that all effects
             # can take place irrespective of ordering
             if cell_arr[i,cell_state] == th1_idx
-                if (1-(cdf(Gamma(1,1), time-cell_arr[i,last_div]))) < cell_arr[i,prob_div]
-                    # find a dead cell and make it alive
+                #if (1-(cdf(Exponential(1/r_div), time-cell_arr[i,last_div]))) < cell_arr[i,prob_div]
+
+                if cell_arr[i, prob_div0] > cell_arr[i, prob_div]    # find a dead cell and make it alive
                     # from all dead cells start with the first index then increase
                     dead_cells = findall(cell_arr[:,cell_state] .== dead_idx)
                     k = dead_cells[1]
@@ -96,16 +114,21 @@ function stoc_model(n_cells, time_arr)
                     cell_arr[k,last_state_change] = time
                     cell_arr[k,last_div] = time
                     cell_arr[k,last_death] = time
-
+                    cell_arr[k, prob_div0] = 0
                     # for mother cell update division time and assign new division probability
                     cell_arr[i, prob_div] = rand(Float64)
+                    cell_arr[i, prob_div0] = 0
                     cell_arr[i, last_div] = time
+                else
+                    cell_arr[i, prob_div0] = cell_arr[i, prob_div0] +
+                    cdf(Exponential(1/r_div),t_new-cell_arr[i,last_div])-
+                    cdf(Exponential(1/r_div),time-cell_arr[i,last_div])
                 end
             end
             # check if cell state is not naive (0) or dead(8)
             # then check if cell will die
             if cell_arr[i,cell_state] in [th1_idx, tfh_idx, tr1_idx]
-                if (1-(cdf(Gamma(1,1), time-cell_arr[i,last_death]))) < cell_arr[i,prob_death]
+                if (1-(cdf(Exponential(1), time-cell_arr[i,last_death]))) < cell_arr[i,prob_death]
                     cell_arr[i,cell_state] = dead_idx
                     # do I need to do anything with dead cells?
                     # when dead cell comes alive I assign everythin new
@@ -133,10 +156,10 @@ end
 
 
 # parameters
-n_cells = 2000
+n_cells = 1000
 # cell can be alive or dead
 # create cell array
-time_arr = range(0, 50, step = 0.1)
+time_arr = range(0, 10, step = 0.05)
 
 # first entry is cell cell_state
 # second entry is jump time (last state transition)
@@ -149,5 +172,7 @@ end
 
 df = run_sim(200, n_cells, time_arr)
 
-using StatsPlots
-@df df plot(:time, [:Th1 :Tfh], colour = [:red :blue])
+#using StatsPlots
+#@df df plot(:time, [:Th1 :Tfh], colour = [:red :blue])
+
+CSV.write("Onedrive/projects/2020/tcell_stochastic/output/teststoring.csv", df)
