5f230b1efa4883737a1d3590ee3b50238795bd70
burt-sysbio <philippburt@gmail.com>
2020-07-16 10:40:58 +0200

    added heatmap plot and pca plot to stoc sim data, color code missing

--- code/plot_gene_sim.py
+++ code/plot_gene_sim.py
@@ -10,23 +10,53 @@ import numpy as np
 import h5py
 import pandas as pd
 import scanpy as sc
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+from random import randrange
 data = h5py.File('../output/scseq_sim.h5', 'r')
 
 
-th1_genes = ["th1_"+str(i) for i in range(5)]
-tfh_genes = ["th1_"+str(i) for i in range(5)]
-tr1_genes = ["tr1_"+str(i) for i in range(5)]
+th1_genes = ["th1_"+str(i) for i in range(20)]
+tfh_genes = ["th1_"+str(i) for i in range(20)]
+tr1_genes = ["tr1_"+str(i) for i in range(20)]
 
 colnames = th1_genes + tfh_genes + tr1_genes
 
 df_list = [pd.DataFrame(data[key]) for key in data.keys()]
+data.close()
 
 
 df = df_list[0]
+# set elements smaller than zero to 0 but I have to check why they arose in the first place
+df[df<0] = 0
 adata = sc.AnnData(df.T)
-#adata.var = colnames
-data.close()
+adata.var_names = colnames
 
-sc.pp.normalize_total(adata, target_sum=1e4)
-sc.pp.log1p(adata)
+# create UMIs
+obs = ["UMI"+str(np.random.randint(1000,9999)) for i in range(1898)]
+adata.obs_names = obs
+
+sc.pl.highest_expr_genes(adata, n_top=20, )
+
+adata_th1 = adata[:,:20]
+adata_tfh = adata[:,20:40]
+adata_tr1 = adata[:,40:60]
+
+cpc_th1 = adata_th1.X.sum(axis = 1)
+cpc_tfh = adata_tfh.X.sum(axis = 1)
+cpc_tr1 = adata_tr1.X.sum(axis = 1)
+
+df_cpc = pd.DataFrame({"th1_genes" : cpc_th1, 
+                       "tfh_genes" : cpc_tfh, 
+                       "tr1_genes" : cpc_tr1})
+
+df_cpc = pd.melt(df_cpc)
+
+sns.catplot(data = df_cpc, x = "variable", y = "value", kind = "violin")
+sns.clustermap(adata.X)
+
+
+sc.tl.pca(adata)
+
+sc.pl.pca_variance_ratio(adata, log=True)
+sc.pl.pca(adata, color = ["th1_1"])
